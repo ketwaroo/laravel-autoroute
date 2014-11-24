aa3f081123550e9eb054cf20a0ac33b7be325c51
Yaasir Ketwaroo <ketwaroo.yaasir@gmail.com>
2014-11-24 18:35:41 -0500

    initial commit.

---
+++ src/Ketwaroo/Autoroute/AutorouteServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Ketwaroo\Autoroute;
+
+use Illuminate\Support\ServiceProvider;
+
+class AutorouteServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('ketwaroo/autoroute');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
